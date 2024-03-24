46943c28f638c45592acb1465d47cd80fa4c131c
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-03-24 20:14:39 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+
+function CustomerName() {
+  customerName = customerName.toUpperCase()
+
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+const leastFavoriteCustomer = 'some initial value'
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy'
+}
